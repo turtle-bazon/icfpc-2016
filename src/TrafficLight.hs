9da18b5b4979c179e4f942a6fcca9e617cf3dc69
Alexey Voznyuk <me@swizard.info>
2016-07-30 02:22:31 +0300

    directions enum

--- src/TrafficLight.hs
+++ src/TrafficLight.hs
@@ -1,4 +1,19 @@
 module TrafficLight (countValid) where
 
+data Direction
+    = SouthToEast
+    | SouthToNorth
+    | SouthToWest
+    | EastToNorth
+    | EastToWest
+    | EastToSouth
+    | NorthToWest
+    | NorthToSouth
+    | NorthToEast
+    | WestToNorth
+    | WestToEast
+    | WestToSouth
+
+
 countValid :: String
 countValid = "dummy"
