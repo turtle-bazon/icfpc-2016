ca457fe57e778d8feebb96e8731c7d76a9823bdb
Alexey Voznyuk <me@swizard.info>
2016-07-30 02:32:44 +0300

    initial allowed directions

--- src/TrafficLight.hs
+++ src/TrafficLight.hs
@@ -15,5 +15,17 @@ data Direction
     | WestToSouth
 
 
+allowedPair :: Direction -> Direction -> Bool
+allowedPair SouthToEast WestToEast = False
+allowedPair SouthToEast _ = True
+allowedPair SouthToNorth SouthToEast = True
+allowedPair SouthToNorth SouthToWest = True
+allowedPair SouthToNorth NorthToSouth = True
+allowedPair SouthToNorth NorthToWest = True
+allowedPair SouthToNorth WestToSouth = True
+allowedPair SouthToNorth _ = False
+allowedPair _ _ = False
+
+
 countValid :: String
 countValid = "dummy"
