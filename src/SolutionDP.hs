4197868eeec00b949d6644f7dff0766c882173e4
Alexey Voznyuk <me@swizard.info>
2016-07-30 16:05:27 +0300

    reverse parameters order

--- src/SolutionDP.hs
+++ src/SolutionDP.hs
@@ -10,8 +10,8 @@ initialState cfg = [cfg]
 totalAmount :: Config -> Int
 totalAmount = sum . map amount
 
-trafficRun :: GreenLights -> Int -> Config -> Config
-trafficRun lights duration = map runLights
+trafficRun :: Int -> GreenLights -> Config -> Config
+trafficRun duration lights = map runLights
     where
       runLights flow | route flow `elem` lights = decr flow
       runLights flow = flow
