b8e87919008b9f9924884ede97f1d8607eccf45b
Alexey Voznyuk <me@swizard.info>
2016-07-30 15:55:13 +0300

    `totalAmount` and `minuteRun`

--- src/SolutionDP.hs
+++ src/SolutionDP.hs
@@ -2,6 +2,20 @@ module SolutionDP where
 
 import TrafficLight
 
+type States = [Config]
+
+initialState :: Config -> States
+initialState cfg = [cfg]
+
+totalAmount :: Config -> Int
+totalAmount = sum . map amount
+
+minuteRun :: GreenLights -> Config -> Config
+minuteRun lights = map runLights
+    where
+      runLights flow | route flow `elem` lights = flow { amount = (amount flow) - (throughput flow) }
+      runLights flow = flow
+
 solution :: Config -> Int
 solution cfg =
     42
