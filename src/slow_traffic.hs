0fd10b094db5a3850d83ce8f7f86c51aa31221a7
Azamat S. Kalimoulline <turtle@bazon.ru>
2016-08-02 22:18:26 +0300

    Several methodics.

--- src/slow_traffic.hs
+++ src/slow_traffic.hs
@@ -2,7 +2,8 @@ module Main where
 
 import Data.Bits (testBit)
 import Data.Function (on)
-import Data.List (find, minimumBy, sortBy)
+import Data.List (elemIndex, find, minimumBy, sortBy)
+import System.Environment (getArgs)
 
 data CrossRoads = CrossRoads {velocities :: [Int],
                               variant :: Int,
@@ -60,6 +61,12 @@ chooseLightsDev cars lights =
 isRightTurn :: Int -> Bool
 isRightTurn lightNumber = elem lightNumber [1, 4, 7, 10]
 
+isStraight :: Int -> Bool
+isStraight lightNumber = elem lightNumber [2, 5, 8, 11]
+
+isLeftTurn :: Int -> Bool
+isLeftTurn lightNumber = elem lightNumber [3, 6, 9, 12]
+
 lightCompareAll :: (Int, Int) -> (Int, Int) -> Ordering
 lightCompareAll (lightNumber1, cars1) (lightNumber2, cars2)
   | cars1 == 0 = LT
@@ -72,9 +79,27 @@ lightCompareExceptTurnRight (lightNumber1, cars1) (lightNumber2, cars2)
   | not (isRightTurn lightNumber1) && isRightTurn lightNumber2 = GT
   | otherwise = lightCompareAll (lightNumber1, cars1) (lightNumber2, cars2)
 
+lightCompare :: (Int, Int) -> (Int, Int) -> Ordering
+lightCompare (lightNumber1, cars1) (lightNumber2, cars2)
+  | cars1 == 0 = LT
+  | cars2 == 0 = GT
+--  | isRightTurn lightNumber1 && not (isRightTurn lightNumber2) = LT
+--  | not (isRightTurn lightNumber1) && isRightTurn lightNumber2 = GT
+--  | isLeftTurn lightNumber1 && not (isLeftTurn lightNumber2) = GT
+--  | not (isLeftTurn lightNumber1) && isLeftTurn lightNumber2 = LT
+  | otherwise = compare cars1 cars2
+
+compareLight :: Int -> Int -> Ordering
+compareLight l1 l2 =
+  EQ
+
+lightComparePriority :: (Int, Int) -> (Int, Int) -> Ordering
+lightComparePriority (lightNumber1, cars1) (lightNumber2, cars2) =
+  EQ
+
 chooseLights :: Int -> Int -> [Int] -> ([Int], [Int])
 chooseLights variant currentMinute cars =
-  let lightCompare = if (testBit variant (currentMinute - 1)) then lightCompareExceptTurnRight else lightCompareAll
+  let --lightCompare = if (testBit variant (currentMinute - 1)) then lightCompareExceptTurnRight else lightCompareAll
       sortedCars = sortBy (flip lightCompare) $ zip [1..] cars
       devLights = chooseLightsDev sortedCars []
   in (map (\lightNumber -> if (elem lightNumber devLights) then 1 else 0) [1..12], sortBy compare devLights)
@@ -97,7 +122,7 @@ parseTask string_cars string_velocities =
 
 solve :: CrossRoads -> CrossRoads
 solve cr
-  | currentMinute cr == 10 = cr
+  | currentMinute cr == 2 = cr
   | otherwise = solve $ evaluateMinute cr $ chooseLights (variant cr) (currentMinute cr) (cars $ head $ minutes cr)
 
 constructVariant :: (CrossRoads, Int) -> CrossRoads
@@ -113,12 +138,14 @@ compareSolutions cr1 cr2 =
 
 main :: IO ()
 main = do
+  cmdArgs <- getArgs
   l1 <- getLine
   l2 <- getLine
   let cr = parseTask l1 l2
-      cr_variants = map constructVariant $ zip (repeat cr) [0..1023]
+      cr_variants = map constructVariant $ zip (repeat cr) [0..0]
       cr_solutions = map solve cr_variants
       best_solution = minimumBy compareSolutions cr_solutions
     in
-    --print best_solution
-    print $ maximum $ cars $ head $ minutes best_solution
+    case cmdArgs of
+      ["print"] -> print best_solution
+      [] -> print $ maximum $ cars $ head $ minutes best_solution
