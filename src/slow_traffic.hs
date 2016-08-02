f04f3bf81243ba57058310e6a0f06f39b1fba0d4
Azamat S. Kalimoulline <turtle@bazon.ru>
2016-08-02 16:06:00 +0300

    Bruteforce with light choosing method.

--- src/slow_traffic.hs
+++ src/slow_traffic.hs
@@ -1,15 +1,18 @@
 module Main where
 
+import Data.Bits (testBit)
 import Data.Function (on)
-import Data.List (find, sortBy)
+import Data.List (find, minimumBy, sortBy)
 
 data CrossRoads = CrossRoads {velocities :: [Int],
+                              variant :: Int,
                               currentMinute :: Int,
                               minutes :: [CrossRoadsMinute]} deriving (Show)
 
 data CrossRoadsMinute = CrossRoadsMinute {minute :: Int,
                                           cars :: [Int],
-                                          traffic_lights :: [Int]} deriving (Show)
+                                          traffic_lights :: [Int],
+                                          debug_lights :: [Int]} deriving (Show)
 
 applyVelocity :: Int -> Int -> Int
 applyVelocity car velocity = max 0 (car - velocity)
@@ -18,14 +21,16 @@ applyLights :: [Int] -> [Int] -> [Int] -> [Int]
 applyLights lights velocities cars =
   zipWith applyVelocity cars $ zipWith (*) lights velocities
 
-evaluateMinute :: CrossRoads -> [Int] -> CrossRoads
-evaluateMinute cr tl =
+evaluateMinute :: CrossRoads -> ([Int], [Int]) -> CrossRoads
+evaluateMinute cr (tl, dl) =
   let now = currentMinute cr + 1
   in CrossRoads {velocities = velocities cr,
+                 variant = variant cr,
                  currentMinute = now,
                  minutes = CrossRoadsMinute {minute = now,
                                              cars = applyLights tl (velocities cr) (cars $ head $ minutes cr),
-                                             traffic_lights = tl} : minutes cr}
+                                             traffic_lights = tl,
+                                             debug_lights = dl} : minutes cr}
 
 isCrossing :: Int -> Int-> Bool
 isCrossing routeA routeB =
@@ -52,11 +57,27 @@ chooseLightsDev cars lights =
      then lights
      else (route : lights))
 
-chooseLights :: [Int] -> [Int]
-chooseLights cars =
-  let sortedCars = sortBy ((flip  compare) `on` snd) $ zip [1..] cars
+isRightTurn :: Int -> Bool
+isRightTurn lightNumber = elem lightNumber [1, 4, 7, 10]
+
+lightCompareAll :: (Int, Int) -> (Int, Int) -> Ordering
+lightCompareAll (lightNumber1, cars1) (lightNumber2, cars2)
+  | cars1 == 0 = LT
+  | cars2 == 0 = GT
+  | otherwise = compare cars1 cars2
+
+lightCompareExceptTurnRight :: (Int, Int) -> (Int, Int) -> Ordering
+lightCompareExceptTurnRight (lightNumber1, cars1) (lightNumber2, cars2)
+  | isRightTurn lightNumber1 && not (isRightTurn lightNumber2) = LT
+  | not (isRightTurn lightNumber1) && isRightTurn lightNumber2 = GT
+  | otherwise = lightCompareAll (lightNumber1, cars1) (lightNumber2, cars2)
+
+chooseLights :: Int -> Int -> [Int] -> ([Int], [Int])
+chooseLights variant currentMinute cars =
+  let lightCompare = if (testBit variant (currentMinute - 1)) then lightCompareExceptTurnRight else lightCompareAll
+      sortedCars = sortBy (flip lightCompare) $ zip [1..] cars
       devLights = chooseLightsDev sortedCars []
-  in map (\lightNumber -> if (elem lightNumber devLights) then 1 else 0) [1..12]
+  in (map (\lightNumber -> if (elem lightNumber devLights) then 1 else 0) [1..12], sortBy compare devLights)
 
 parseList :: [Char] -> [Int]
 parseList st =
@@ -67,19 +88,37 @@ parseList st =
 parseTask :: [Char] -> [Char] -> CrossRoads
 parseTask string_cars string_velocities =
   CrossRoads {velocities = parseList string_velocities,
+              variant = 0,
               currentMinute = 0,
               minutes = [CrossRoadsMinute {minute = 0,
                                            cars = parseList string_cars,
-                                           traffic_lights = map (const 0) [1..12]}]}
+                                           traffic_lights = map (const 0) [1..12],
+                                           debug_lights = []}]}
 
 solve :: CrossRoads -> CrossRoads
 solve cr
   | currentMinute cr == 10 = cr
-  | otherwise = solve $ evaluateMinute cr $ chooseLights $ cars $ head $ minutes cr
+  | otherwise = solve $ evaluateMinute cr $ chooseLights (variant cr) (currentMinute cr) (cars $ head $ minutes cr)
+
+constructVariant :: (CrossRoads, Int) -> CrossRoads
+constructVariant (cr, variant) =
+  CrossRoads {velocities = velocities cr,
+              variant = variant,
+              currentMinute = 0,
+              minutes = minutes cr}
+
+compareSolutions :: CrossRoads -> CrossRoads -> Ordering
+compareSolutions cr1 cr2 =
+  compare (maximum $ cars $ head $ minutes $ cr1) (maximum $ cars $ head $ minutes $ cr2)
 
 main :: IO ()
 main = do
   l1 <- getLine
   l2 <- getLine
-  let cr = parseTask l1 l2 in
-    print $ maximum $ cars $ head $ minutes $ solve cr
+  let cr = parseTask l1 l2
+      cr_variants = map constructVariant $ zip (repeat cr) [0..1023]
+      cr_solutions = map solve cr_variants
+      best_solution = minimumBy compareSolutions cr_solutions
+    in
+    --print best_solution
+    print $ maximum $ cars $ head $ minutes best_solution
