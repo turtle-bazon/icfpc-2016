b91e54348bb05060154a06f598e05eefedece349
Alexey Voznyuk <me@swizard.info>
2016-08-05 15:28:01 +0300

    initial solution

--- src/Common.hs
+++ src/Common.hs
@@ -26,6 +26,19 @@ type FacetPoly = [PointIndex]
 
 data Solution = Solution { src :: [IndexedPoint], facets :: [FacetPoly], dst :: [Point] } deriving (Eq, Show)
 
+initSolution :: Solution
+initSolution =
+    Solution { src = [ IndexedPoint { index = 0, vertex = Point { px = 0, py = 0 } }
+                     , IndexedPoint { index = 1, vertex = Point { px = 1, py = 0 } }
+                     , IndexedPoint { index = 2, vertex = Point { px = 1, py = 1 } }
+                     , IndexedPoint { index = 3, vertex = Point { px = 0, py = 1 } }
+                     ],
+               facets = [[0, 1, 2, 3]],
+               dst = [ Point { px = 0, py = 0 }
+                     , Point { px = 1, py = 0 }
+                     , Point { px = 1, py = 1 }
+                     , Point { px = 0, py = 1 }
+                     ] }
 
 polygon :: SilhouettePoly -> Poly
 polygon (PolyFill points) = points
