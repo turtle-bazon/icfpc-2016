c9967b87fab51a7d3f52a0826bc73f0512efbbf2
Alexey Voznyuk <me@swizard.info>
2016-08-06 00:47:10 +0300

    scoring works at last

--- src/Figures.hs
+++ src/Figures.hs
@@ -4,6 +4,7 @@ import Data.Ratio
 import Algebra.Clipper
 import Common
 import Math
+import Parse
 
 toIntPoint :: Point -> IntPoint
 toIntPoint p =
@@ -40,6 +41,9 @@ polyIntersect = polyOp intersection
 polyUnion :: [Poly] -> [Poly] -> IO [Poly]
 polyUnion = polyOp union
 
+polyDifference :: [Poly] -> [Poly] -> IO [Poly]
+polyDifference = polyOp difference
+
 polyArea :: Poly -> IO Double
 polyArea p = do
   let poly = toIntPoly p
@@ -55,3 +59,24 @@ silhouetteArea sil = do
           area' (PolyHole poly) = do
                        a <- polyArea poly
                        return $ -a
+
+silhouetteToPolygons :: Silhouette -> IO [Poly]
+silhouetteToPolygons sil =
+    traverse' sil []
+        where
+          traverse' [] ready = return $ ready
+          traverse' ((PolyFill poly) : rest) ready = do
+            merged <- polyUnion [poly] ready
+            traverse' rest merged
+          traverse' ((PolyHole poly) : rest) ready = do
+            diffed <- polyDifference ready [poly]
+            traverse' rest diffed
+
+score :: Silhouette -> [Poly] -> IO Double
+score sil facets = do
+  siPolys <- silhouetteToPolygons sil
+  pIntersect <- polyIntersect facets siPolys
+  pUnion <- polyUnion facets siPolys
+  pIntersectAreas <- traverse polyArea pIntersect
+  pUnionAreas <- traverse polyArea pUnion
+  return $ (sum pIntersectAreas) / (sum pUnionAreas)
