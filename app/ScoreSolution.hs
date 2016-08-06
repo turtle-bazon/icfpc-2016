7c8b16929e7a9af07760a8ba71c6991d9392b658
Gleb Golubitsky <sectoid@gnolltech.org>
2016-08-06 22:00:53 +0300

    submit: scoring CLI tool
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

---
+++ app/ScoreSolution.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import System.Environment
+import Common
+import Figures
+import SolversRun
+
+main :: IO ()
+main = do
+  args <- getArgs
+  processArgs args
+      where
+        processArgs (problemFile : solutionFile : _) = do
+          problem <- loadProblem $ problemFile
+          solution <- loadSolution $ solutionFile
+          sim <- score (silhouette problem) solution
+          putStrLn $ show sim
+        processArgs _ = do
+          processName <- getProgName
+          putStrLn $ "Usage: " ++ processName ++ " <problem file> <solution file>"
+  
+  
