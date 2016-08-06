3d448e1da1162a47e7903e9b9779fcc0214eea24
Alexey Voznyuk <me@swizard.info>
2016-08-07 02:47:18 +0300

    centrify solution

--- src/SolverRandomSearch.hs
+++ src/SolverRandomSearch.hs
@@ -1,3 +1,32 @@
-module SolverRandomSearch where
+module SolverRandomSearch (solverRandomSearch) where
 
 import System.Random
+import Common
+import Math
+import SolverBBSimple
+import Parse
+
+centrifyPos :: Problem -> Point
+centrifyPos = centrifyPoly . parseFirstPoly . silhouette
+
+centrifyPoly :: Poly -> Point
+centrifyPoly = center . bbox
+    where
+      center (Point { px = lbx, py = lby }, Point { px = rtx, py = rty }) =
+          Point { px = lbx + (rtx - lbx) / 2, py = lby + (rty - lby) / 2 }
+
+dstPoly :: Solution -> Poly
+dstPoly = map dstvertex . points
+
+centrify :: Problem -> Solution -> Solution
+centrify problem solution =
+    translate relativeCenter solution
+        where
+          relativeCenter = translatePoint solutionDelta problemCenter
+          problemCenter = centrifyPos problem
+          solutionDelta = negatePoint $ centrifyPoly $ dstPoly solution
+
+
+
+solverRandomSearch :: Problem -> IO Solution
+solverRandomSearch = undefined
