d9832dfca48692c3eb168fcfc798931a892f97c2
Alexey Voznyuk <me@swizard.info>
2016-08-06 18:03:46 +0300

    solver run library

---
+++ src/SolverBBSimple.hs
@@ -0,0 +1,31 @@
+module SolverBBSimple (solverBBSimple) where
+
+import Data.Ratio
+import Common
+import Math
+
+bbox :: Poly -> (Point, Point)
+bbox points =
+    (topLeft, bottomRight)
+        where
+          topLeft = Point { px = minX, py = minY }
+          bottomRight = Point { px = maxX, py = maxY }
+          minX = minimum $ map px points
+          minY = minimum $ map py points
+          maxX = maximum $ map px points
+          maxY = maximum $ map py points
+
+makeSolution :: Poly -> Solution
+makeSolution points =
+    translate topLeft initSolution
+        where
+          (topLeft, bottomRight) = bbox points
+
+parseFirstPoly :: Silhouette -> Poly
+parseFirstPoly = polygon . head . filter isFillPoly
+    where
+      isFillPoly (PolyFill _) = True
+      isFillPoly (PolyHole _) = False
+
+solverBBSimple :: Problem -> Solution
+solverBBSimple = makeSolution . parseFirstPoly . silhouette
---
+++ src/SolversRun.hs
@@ -0,0 +1,62 @@
+module SolversRun where
+
+import Data.List
+import System.IO
+import System.Directory
+import Control.DeepSeq
+import Common
+import Parse
+import Figures
+import Show
+import SolverBBSimple
+
+data Solver = Solver { name :: String, solve :: Problem -> Solution }
+
+data Rating = Rating { solverName :: String, solution :: Solution, similarity :: Double }
+
+solvers :: [Solver]
+solvers = [ Solver { name = "simple bbox", solve = solverBBSimple }
+          ]
+
+loadProblem :: String -> IO Problem
+loadProblem filename = do
+    fd <- openFile filename ReadMode
+    contents <- hGetContents fd
+    contents `deepseq` hClose fd
+    return $ parseProblem $ lines contents
+
+loadAllProblems :: String -> IO [(String, Problem)]
+loadAllProblems dir = do
+  files <- getDirectoryContents dir
+  mapM (\f -> loadProblem (dir ++ "/" ++ f) >>= return . (,) f) $ filter (isSuffixOf ".txt") files
+
+rateSolver :: Problem -> Solver -> IO Rating
+rateSolver problem solver = do
+  sim <- score (silhouette problem) solution
+  return $ Rating { solverName = name solver
+                  , solution = solution
+                  , similarity = sim
+                  }
+      where
+        solution = solve solver $ problem
+
+rateSolvers :: Problem -> IO [Rating]
+rateSolvers problem = mapM (rateSolver problem) solvers
+
+bestSolver :: [Rating] -> Rating
+bestSolver = head . reverse . sortOn similarity
+
+runProblem :: String -> (String, Problem) -> IO ()
+runProblem outputDir (problemFile, problem) = do
+  ratings <- rateSolvers problem
+  let best = bestSolver ratings
+  let outFile = outputDir ++ "/" ++ problemFile
+  fd <- openFile outFile WriteMode
+  problemContents <- hPutStr fd $ showSolution $ solution best
+  hClose fd
+  putStrLn $ "Task " ++ problemFile ++ ", best score = " ++ (show $ similarity best) ++ " with solver: " ++ (solverName best)
+
+run :: String -> String -> IO ()
+run problemsDir outputDir = do
+  problems <- loadAllProblems problemsDir
+  mapM_ (runProblem outputDir) problems
