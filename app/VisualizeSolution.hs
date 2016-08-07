970ab54b0d50bfc5883723df78c83fe663639ef7
Gleb Golubitsky <sectoid@gnolltech.org>
2016-08-07 05:07:42 +0300

    visualize-solution: problem visualization support
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- app/VisualizeSolution.hs
+++ app/VisualizeSolution.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import System.Environment
 import Graphics.QML
 import Data.IORef
 import Data.Text (Text)
@@ -7,22 +8,34 @@ import qualified Data.Text as T
 
 import Parse
 import Visualize
+import SolversRun
 import Text.JSON
 
 import Paths_icfpc2016
 
+
 main :: IO ()
 main = do
-    solutionData <- getContents
-    solution <- return $ parseSolution $ lines solutionData
-    model <- return $ encode $ makeSolutionModel solution
-    state <- newIORef $ T.pack $ model
-    -- putStrLn model
-    clazz <- newClass [
-      defPropertyConst' "modelJSON" (\_ -> readIORef state)
-      ]
-    ctx <- newObject clazz ()
-    doc <- getDataFileName "solution.qml"
-    runEngineLoop defaultEngineConfig {
-        initialDocument = fileDocument doc,
-        contextObject = Just $ anyObjRef ctx}
+  args <- getArgs
+  processArgs args
+      where
+        processArgs (problemFile : solutionFile : _) = do
+          problem <- loadProblem $ problemFile
+          solution <- loadSolution $ solutionFile
+          model <- return $ encode $ makeSolutionModel solution
+          problemModel <- return $ encode $ makeProblemModel problem
+          state <- newIORef $ T.pack $ model
+          state2 <- newIORef $ T.pack $ problemModel
+          -- putStrLn model
+          clazz <- newClass [
+            defPropertyConst' "modelJSON" (\_ -> readIORef state),
+            defPropertyConst' "problemJSON" (\_ -> readIORef state2)
+            ]
+          ctx <- newObject clazz ()
+          doc <- getDataFileName "solution.qml"
+          runEngineLoop defaultEngineConfig {
+            initialDocument = fileDocument doc,
+            contextObject = Just $ anyObjRef ctx}
+        processArgs _ = do
+          processName <- getProgName
+          putStrLn $ "Usage: " ++ processName ++ " <problem file> <solution file>"
