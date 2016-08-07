48c0ce01bb2373f89a3dbcd0ed118a1d595393b4
Gleb Golubitsky <sectoid@gnolltech.org>
2016-08-07 19:38:13 +0300

    revert temporate changes
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- tools/submit.php
+++ tools/submit.php
@@ -119,7 +119,7 @@ function submitSolution($solution) {
 
     /* Delay */
     $now = microtime(true);
-    $delay = $lastQuery - $now + 1.1;
+    $delay = $lastQuery - $now + 1.5;
     if ($delay > 0) {
       usleep((int)$delay*1000000);
     }
