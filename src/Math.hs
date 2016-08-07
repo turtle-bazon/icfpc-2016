52cff4b9b052da49aa0db294417d5a23d8d7466f
Azamat S. Kalimoulline <turtle@bazon.ru>
2016-08-07 03:49:20 +0300

    ReflectPoint function

--- src/Math.hs
+++ src/Math.hs
@@ -377,6 +377,23 @@ foldLeft x (Solution points facets) =
   in normalizeSolution Solution { points = newPoints
                                 , facets = newFacets}
 
+reflectPointByEdge :: Edge -> Point -> Point
+reflectPointByEdge ((Point x1 y1), (Point x2 y2)) =
+  let
+    dx = x2 - x1
+    dy = y2 - y1
+    n = dx
+    m = dy
+    b = dx * y1 - dy * x1
+    pivot = Point { px = 0, py = b }
+    toOrigin = translatePoint (negatePoint pivot)
+    fromOrigin = translatePoint pivot
+    reflect p = let k = 1 / (n * n + m * m)
+                in Point { px = k * ((px p) * (1 - m * m) + (py p) * 2 *m)
+                         , py = k * ((px p) * 2 * m + (py p) * (m * m - 1))
+                         }
+  in fromOrigin . reflect . toOrigin
+  
 foldByEdge :: Edge -> Solution -> Solution
 foldByEdge edge solution =
     undefined
